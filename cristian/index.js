7305efdc1945f7e19e4cd0f6cc9213d5c7138ce0
Cristian Bello <cristian.bello6@gmail.com>
2021-07-13 21:27:17 -0500

    ajuste al ejercicio de los angulos

--- cristian/index.js
+++ cristian/index.js
@@ -1,13 +1,20 @@
 console.log("cristian/index/js");
 
 function angulos(grados) {
-  if(grados >=0 && grados < 90){
+ if(grados >=0 && grados < 90){
     console.log("Angulo Agudo");
-  }else if(grados >= 90 && grados <180){
+  }else if(grados > 90 && grados <180){
     console.log("Angulo obtuso");
-  }else if(grados == 90 && grados == 180){
+  }else if(grados == 90){
     console.log("Angulo recto");
+  }else if (grados ==180) {
+    console.log("Angulo llano");
   }
-}
+} 
+
+angulos(80);
+angulos(120);
+angulos(90);
+angulos(180);
 
 
