d21bef6fecc109b52ab1545738ac145c39c49cc8
Cristian Bello <cristian.bello6@gmail.com>
2021-07-13 20:34:41 -0500

    creación ejercicio angulos

---
+++ cristian/index.js
@@ -0,0 +1,13 @@
+console.log("cristian/index/js");
+
+function angulos(grados) {
+  if(grados >=0 && grados < 90){
+    console.log("Angulo Agudo");
+  }else if(grados >= 90 && grados <180){
+    console.log("Angulo obtuso");
+  }else if(grados == 90 && grados == 180){
+    console.log("Angulo recto");
+  }
+}
+
+
