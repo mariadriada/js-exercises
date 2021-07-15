9e22287985bd08b3dcaeabf6a83c574cda5ebf7a
laura <ll.calderon14@gmail.com>
2021-07-14 19:04:16 -0500

    Realizacion primer ejercicio letras mayusculas

---
+++ laura/index.js
@@ -0,0 +1,14 @@
+console.log("laura/index/js");
+
+
+    function ponerMayuscula(string){
+         return string[0].toUpperCase()+string.slice(1);
+    }
+console.log(ponerMayuscula(`mayuscula`))
+
+
+    
+
+
+
+
