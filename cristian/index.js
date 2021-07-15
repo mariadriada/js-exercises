dd2ed82c1a454389345da5bf4d1560a0b6a85c87
Cristian Bello <cristian.bello6@gmail.com>
2021-07-15 17:58:18 -0500

    ejercicios de practica hasta el 6

--- cristian/index.js
+++ cristian/index.js
@@ -34,3 +34,70 @@ let str = ['hola', 'este', 'es', 'arreglo', 'javascript','ohbraveNewworld'];
 let str2 = ['a','bc','def'];
 console.log(findLongestWord(str));
 console.log(findLongestWord(str2));
+
+
+function multiplicacion(num1, num2){
+  let result = Number(num1 * num2);
+  return result;
+}
+
+console.log(multiplicacion(5,4));
+
+function division(a,b){
+  let result = Number(a/b);
+  return result;
+}
+
+console.log(division(40,5));
+
+function bisiesto(year) {
+ if ((year%4==0) && (year % 100 !=0 || year % 400==0)) {
+   return 'año bisiesto';
+ }else{
+   return 'año no bisiesto';
+ }
+}
+
+console.log(bisiesto(2008));
+console.log(bisiesto(2004));
+console.log(bisiesto(2012));
+console.log(bisiesto(2019));
+
+function triangleArea(l1,l2,l3) {
+  let s = Number((l1+l2+l3)/2.0);
+  let area = Math.sqrt(s*(s-l1)*(s-l2)*(s-l3));
+  return area;
+}
+
+console.log(triangleArea(5,6,7));
+
+function isEven(num){
+  if (num%2===0) {
+    return 'Numero Par'
+  }else{
+    return 'Numero Impar'
+  }
+}
+console.log("--- Numeros Pares ----")
+console.log(isEven(21));
+
+function numbers(num1,num2){
+  if( num1===50 || (num1+num2)===50){
+    return 'verdadero';
+  }else{
+    return 'falso';
+  }
+}
+
+console.log('------numeros 50/50--------');
+console.log(numbers(50,0));
+
+function rangeNumber(number) {
+if(number>= 20 && number<=100){
+ return 'dentro del rango';
+} else{
+  return 'fuera del rango';
+}
+}
+console.log('---------------');
+console.log(rangeNumber(101));
